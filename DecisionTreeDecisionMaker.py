9f84c7da48d9ef4daeaa4129ec3077e0488a77b0
nathan <nathan.gagnon@tfc.com>
2016-06-28 11:41:23 -0400

    moved the decision tree play game code out of the DecisionTreeDecisionMaker class file

--- DecisionTreeDecisionMaker.py
+++ DecisionTreeDecisionMaker.py
@@ -139,18 +139,3 @@ class DecisionTreeDecisionMaker:
         print choice
     return choice
 
-dm = DecisionTreeDecisionMaker()
-
-game = Game()
-game.decisionMaker = dm
-DataManager.settings['gameResultsDbPath'] = 'decisionTreeGameOut.db'
-for i in range(len(game.playerCardDeck)):
-  for j in range(10):
-    if j%100 == 0:
-      print "Player%d - round %d"%(i+1,j)
-    game.resetGame()
-    game.addPlayer( "Player%d"%(i+1) )
-    while game.isNextStep():
-      game.performNextStep( dm.makeDecision( game, game.nextStepAvailableActions() ) )
-    DataManager.insertGameLogIntoDb(game.gameLog)
-    print game.players[0].points()
